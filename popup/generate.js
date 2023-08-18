e2533f7f312d824bcf964042f4ef8ad555efa352
Daniel Xu <dxu@dxuuu.xyz>
2023-08-17 23:37:17 -0600

    Refactor generate()

--- popup/generate.js
+++ popup/generate.js
@@ -2,17 +2,24 @@ let container = document.querySelector(".container");
 let button = document.querySelector("#generate-button");
 let input = document.querySelector("#description-input");
 
-function generate() {
-  // Grey out the button
+// Grey out the button
+function disableButton() {
   button.style.backgroundColor = "#ccc";
   button.style.color = "#888";
   button.disabled = true;
+}
 
-  // Replace the input box with "Success!"
+// Replace the input box with `results`
+function showResults(results) {
   const successMessage = document.createElement("div");
   successMessage.style = input.style.cssText;
-  successMessage.innerText = "Success!";
+  successMessage.innerText = results;
   input.replaceWith(successMessage);
+}
+
+function generate() {
+  showResults("Done, yay!");
+  disableButton();
 };
 
 // Add click event listener to the button
